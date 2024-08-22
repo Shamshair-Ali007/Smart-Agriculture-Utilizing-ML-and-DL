cb868f8a3104720a4987a0fbc19f8af6d7eda6e4
Shamshair-Ali007 <44390045+Shamshair-Ali007@users.noreply.github.com>
2024-08-22 18:08:44 +0500

    Create RNN.py

---
+++ RNN.py
@@ -0,0 +1,25 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+def build_rnn(input_shape, num_classes):
+    model = Sequential()
+    model.add(SimpleRNN(64, activation='tanh', input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+    return model
+
+def train_rnn(X_train, y_train, input_shape, num_classes):
+    model = build_rnn(input_shape, num_classes)
+    model.fit(X_train, y_train, epochs=50, batch_size=32)
+    return model
+
+def evaluate_rnn(model, X_test, y_test):
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+    y_pred = model.predict(X_test)
+    precision = precision_score(y_test.argmax(axis=1), y_pred.argmax(axis=1), average='weighted')
+    recall = recall_score(y_test.argmax(axis=1), y_pred.argmax(axis=1), average='weighted')
+    f1 = f1_score(y_test.argmax(axis=1), y_pred.argmax(axis=1), average='weighted')
+    return accuracy, precision, recall, f1
